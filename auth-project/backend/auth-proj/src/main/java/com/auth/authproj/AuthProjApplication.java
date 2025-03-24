7f1e4c89295cd1b994a9047a9f94ba102eeac91b
Robert Nitu <niturobert02@yahoo.com>
2025-03-24 18:19:00 +0200

    Added auth-proj backend empty spring boot project

---
+++ auth-project/backend/auth-proj/src/main/java/com/auth/authproj/AuthProjApplication.java
@@ -0,0 +1,13 @@
+package com.auth.authproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthProjApplication.class, args);
+    }
+
+}
---
+++ auth-project/backend/auth-proj/src/test/java/com/auth/authproj/AuthProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.authproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
