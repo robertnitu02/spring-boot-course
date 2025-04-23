df25b612690caec96827c3f28fcf78ef44ff771b
Robert Nitu <niturobert02@yahoo.com>
2025-04-23 16:06:43 +0300

    Fix swagger fk

--- auth-project/backend/auth-proj/src/main/java/com/auth/authproj/security/SecurityConfig.java
+++ auth-project/backend/auth-proj/src/main/java/com/auth/authproj/security/SecurityConfig.java
@@ -31,6 +31,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         request -> request.requestMatchers(
                                 "/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
                                 "/swagger-resources",
                                 "/swagger-resources/**",
                                 "/configuration/ui",
